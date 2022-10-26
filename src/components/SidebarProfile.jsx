123fffb984029421cf0f6b51d15804e2efd713ad
xShiro2 <daryllejumalon16@gmail.com>
2022-10-26 13:14:39 +0800

    chore: use axios request instead of fetch

--- src/components/SidebarProfile.jsx
+++ src/components/SidebarProfile.jsx
@@ -1,22 +1,32 @@
 // an attemp fix for stopping the component from rerendering
 // STILL DOESNT WORK
 import React, { useState, useEffect } from "react"
+import { axiosRequest } from "api"
+import { useNavigate } from "react-router-dom"
+
 function SidebarProfile() {
-    const [error, setError] = useState(null);
-    const [data, setData] = useState([]);
-  
+    const [user, setUser] = useState([]);
+    const url = '/api/v1/user/admin'
+    const navigate = useNavigate();
+
     // from react docs: request on load 
     useEffect(() => {
-      fetch("/api/v1/user")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setData(result.data);
-          },
-          (error) => {
-            setError(error);
-          }
-        )
+      const getUser = async() => {
+        try {
+            const response = await axiosRequest.get(url)
+
+            const { status, data } = response
+
+            if (status === 200) {
+              setUser(data.data)
+  
+            }
+        } catch (e) {
+            navigate('/404')
+        }
+      }
+
+      getUser()
     }, [])
 
     return (
@@ -26,8 +36,8 @@ function SidebarProfile() {
             className="rounded-full w-44 h-44 border-2 border-gray-500 object-cover mb-4"
             alt=""
           />
-          <h1 className="font-bold">{data.first_name} {data.last_name}</h1>
-          <h2 className="text-sm">{data.email}</h2>
+          <h1 className="font-bold">{user.first_name} {user.last_name}</h1>
+          <h2 className="text-sm">{user.email}</h2>
         </div>
     )
 }
