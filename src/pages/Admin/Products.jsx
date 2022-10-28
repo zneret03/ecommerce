82531e122157c08566ea1d2a609ab083668c8bcf
xShiro2 <daryllejumalon16@gmail.com>
2022-10-28 08:06:26 +0800

    feat: add delete product

--- src/pages/Admin/Products.jsx
+++ src/pages/Admin/Products.jsx
@@ -1,29 +1,34 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
+import { Button } from "@mui/material"
 import { PrivateLayout, Table } from "components"
+import { axiosRequest } from "api"
+import swal from "sweetalert2"
 
 export default function Products() {
-
   const url = "/api/v1/admin/product";
-
-  // from react docs: request on load 
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
+  const [rows, setRows] = useState([]);
 
   useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setData(result.data);
-        },
-        (error) => {
-          setError(error);
-        }
-      )
+    const getProducts= async() => {
+      try {
+          const response = await axiosRequest.get(url)
+
+          const { status, data } = response
+
+          if (status === 200) {
+            setRows(data.data)
+          }
+
+      } catch (error) {
+        const { status } = error.response
+        console.log(status)
+      }
+    }
+
+    getProducts()
   }, [])
 
-  const rows = data
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
     {
@@ -62,20 +67,71 @@ export default function Products() {
     {
       field: "quantity",
       headerName: "Quantity",
+      type: "number",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "dateCreated",
+      headerName: "Date Created",
       type: "string",
       width: 150,
       editable: true,
     },
-    // {
-    //   field: "fullName",
-    //   headerName: "Full name",
-    //   description: "This column has a value getter and is not sortable.",
-    //   sortable: false,
-    //   width: 160,
-    //   valueGetter: (params) =>
-    //     `${params.row.productName || ""} ${params.row.title || ""}`,
-    // },
+    {
+      field: "dateUpdated",
+      headerName: "Date Updated",
+      type: "string",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "deleteButton",
+      headerName: "Action",
+      sortable: false,
+      width: 160,
+      renderCell: (params) => {
+        return (
+          <Button
+            onClick={(e) => deleteCategory(e, params.row)}
+            variant="contained"
+            color="error"
+          >
+            Delete
+          </Button>
+        );
+      }
+    }
   ]
+
+  const removeRow = (id) => {
+    const newRow = rows.filter((rows) => rows.id !== id);
+ 
+    setRows( newRow );
+  };
+
+  const deleteCategory = async(e, row) => {
+    e.preventDefault()
+
+    try {
+      const response = await axiosRequest.delete(`${url}/${row.id}`)
+      console.log(response.status)
+      const { status } = response
+      if (status === 200) {
+        removeRow(row.id)
+      }
+
+    } catch (error) {
+      const { status } = error.response
+      if (status === 500) {
+        swal.fire({
+          title: "Oops!! Error 500",
+          text: "server not found",
+          icon: "warning",
+        })
+      }
+    }
+  }
 
   return (
     <PrivateLayout
