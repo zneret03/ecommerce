f433f219c6bef7e6533f6e8ba70c249f71a186c5
xShiro2 <daryllejumalon16@gmail.com>
2022-10-29 09:26:28 +0800

    fix: change class to className

--- src/components/selectDropdown.jsx
+++ src/components/selectDropdown.jsx
@@ -4,7 +4,7 @@ export default function selectDropdown(props) {
   return (
     <select
       {...props}
-      class="mt-2 bg-white-50 border border-white-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 white:border-gray-600 white:placeholder-gray-400 text-black white:focus:ring-blue-500 white:focus:border-blue-500"
+      className="mt-2 bg-white-50 border border-white-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 white:border-gray-600 white:placeholder-gray-400 text-black white:focus:ring-blue-500 white:focus:border-blue-500"
     >
       {props.children}
     </select>
