6b1817f75a9098d90cfdfe3ef020342afbf3135d
xShiro2 <daryllejumalon16@gmail.com>
2022-10-31 10:42:51 +0800

    fix: button

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -4,12 +4,14 @@ import { ChevronLeft } from "react-feather"
 
 export default function Back({ to }) {
   return (
-    <Link
+    <div className="flex">
+      <Link
       to={to}
       className="flex align-center gap-2 hover:text-amber-600 transition-all"
     >
       <ChevronLeft />
       <span className="font-bold">Back</span>
     </Link>
+    </div>
   )
 }
