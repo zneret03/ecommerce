2619bd45c5a1902fad4a36d04e2fcf8d6281596f
xShiro2 <daryllejumalon16@gmail.com>
2022-10-31 10:16:56 +0800

    fix: fix errors showing on console

--- src/pages/Admin/UpdateProducts.jsx
+++ src/pages/Admin/UpdateProducts.jsx
@@ -22,7 +22,6 @@ export default function UpdateProducts() {
   const prodID = query.pathname.substring(query.pathname.lastIndexOf('/') + 1)
 
   const navigate = useNavigate();
-  const [isLoaded, setLoaded] = useState(false)
 
   const [
     { productName, price, description, gender, category },
@@ -46,7 +45,6 @@ export default function UpdateProducts() {
       gender,
       category,
     }
-    console.log(datas)
     try {
       const response = await axiosRequest.post(`${prod_url}/${prodID}`, datas)
       const { status } = response
@@ -73,36 +71,36 @@ export default function UpdateProducts() {
     let genderList
 
     const getCategory = async () => {
-        try {
-          const response = await axiosRequest.get(url)
+      try {
+        const response = await axiosRequest.get(url)
 
-          const { status, data } = response
+        const { status, data } = response
 
-          if (status === 200) {
-            categoryList = data.data.categoryList
-            genderList = data.data.genderList
-            setCategoryOptions(categoryList)
-            setGenderOptions(genderList)
-            
-          }
+        if (status === 200) {
+          categoryList = data.data.categoryList
+          genderList = data.data.genderList
+          setCategoryOptions(categoryList)
+          setGenderOptions(genderList)
 
-          if (status === 204) {
-            swal.fire({
-              title: "No category available",
-              text: "Please add a category first",
-              icon: "warning",
-            }).then((result) => {
-              if (result.isConfirmed) {
-                navigate('/category')
-              }
-            })
-          }
         }
-        catch (e) {
-          const { status } = e.response
-          console.log(status)
+
+        if (status === 204) {
+          swal.fire({
+            title: "No category available",
+            text: "Please add a category first",
+            icon: "warning",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              navigate('/category')
+            }
+          })
         }
       }
+      catch (e) {
+        const { status } = e.response
+        console.log(status)
+      }
+    }
 
     const getProduct = async () => {
       try {
@@ -122,9 +120,7 @@ export default function UpdateProducts() {
               prod_data.gender = type.id
             }
           })
-          console.log(prod_data)
           setState(prod_data)
-          setLoaded(true)
         }
       }
       catch {
@@ -137,116 +133,114 @@ export default function UpdateProducts() {
 
   }, [navigate, prodID])
 
-  if (isLoaded) {
-    return (
-      <PrivateLayout
-        title="Update Products"
-        description="this page where you can update products"
-      >
-        <Back to="/products" />
-        <section className="py-8">
-          <form onSubmit={(event) => onSubmit(event)}>
-            <div className="mb-6 grid grid-cols-2 gap-10">
-              <div>
-                <div className="mb-6">
-                  <InputField
-                    type="text"
-                    name='productName'
-                    value={productName}
-                    onChange={(event) => onChange(event)}
-                    placeholder="Product Name"
-                    required
-                  />
-                </div>
-  
-                <div className="mb-6">
-                  <textarea
-                    name='description'
-                    value={description}
-                    onChange={(event) => onChange(event)}
-                    placeholder="Description"
-                    required
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-amber-600 focus:outline-none"
-                  />
-                </div>
-  
-                <div className="mb-6">
-                  <InputField
-                    type="number"
-                    name='price'
-                    value={price}
-                    onChange={(event) => onChange(event)}
-                    placeholder="Price"
-                    required
-                  />
-                </div>
-  
-                <div className="mb-6 grid grid-cols-2 gap-2">
-                  <SelectDropdown name="gender" onChange={(event) => onChange(event)}>
-                    <option disabled>
-                      Gender
-                    </option>
-                    {genderOptions.map((type) => {
-                      if (type.id === gender) {
-                        return (
-                          <option selected key={type.id} value={type.id}>{type.gender}</option>
-                        )
-                      }
-                      else {
-                        return (
-                          <option key={type.id} value={type.id}>{type.gender}</option>
-                        )
-                      }
-  
-                    })}
-                  </SelectDropdown>
-  
-                  <SelectDropdown name="category" onChange={(event) => onChange(event)}>
-                    <option disabled>
-                      Category
-                    </option>
-                    {categoryOptions.map((type) => {
-                      if (type.id === category) {
-                        return (
-                          <option selected key={type.id} value={type.id}>{type.categoryName}</option>
-                        )
-                      } else {
-                        return (
-                          <option key={type.id} value={type.id}>{type.categoryName}</option>
-                        )
-                      }
-                    })}
-                  </SelectDropdown>
-                </div>
-  
-                <div className="text-center lg:text-left">
-                  <button
-                    type="submit"
-                    className="inline-block px-7 py-3 bg-amber-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-amber-700 hover:shadow-lg focus:bg-amber-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-amber-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                  >
-                    Update Product
-                  </button>
-                </div>
-  
+  return (
+    <PrivateLayout
+      title="Update Products"
+      description="this page where you can update products"
+    >
+      <Back to="/products" />
+      <section className="py-8">
+        <form onSubmit={(event) => onSubmit(event)}>
+          <div className="mb-6 grid grid-cols-2 gap-10">
+            <div>
+              <div className="mb-6">
+                <InputField
+                  type="text"
+                  name='productName'
+                  value={productName}
+                  onChange={(event) => onChange(event)}
+                  placeholder="Product Name"
+                  required
+                />
               </div>
-  
-              <div>
-                <p>Product Image</p>
-                <div className="flex justify-center items-center w-full">
-                  <label for="dropzone-file" className="flex flex-col justify-center items-center w-full h-64 rounded-lg border-2 border-dashed cursor-pointer hover:border-amber-600">
-                    <div className="flex flex-col justify-center items-center pt-5 pb-6">
-                      <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                    </div>
-                    <input id="dropzone-file" type="file" className="hidden" />
-                  </label>
-                </div>
+
+              <div className="mb-6">
+                <textarea
+                  name='description'
+                  value={description}
+                  onChange={(event) => onChange(event)}
+                  placeholder="Description"
+                  required
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-amber-600 focus:outline-none"
+                />
+              </div>
+
+              <div className="mb-6">
+                <InputField
+                  type="number"
+                  name='price'
+                  value={price}
+                  onChange={(event) => onChange(event)}
+                  placeholder="Price"
+                  required
+                />
+              </div>
+
+              <div className="mb-6 grid grid-cols-2 gap-2">
+                <SelectDropdown name="gender" onChange={(event) => onChange(event)}>
+                <option disabled>
+                    Gender
+                  </option>
+                  {genderOptions.map((type) => {
+                    if (type.id === gender) {
+                      return (
+                        <option selected key={type.id} value={type.id}>{type.gender}</option>
+                      )
+                    }
+                    else {
+                      return (
+                        <option key={type.id} value={type.id}>{type.gender}</option>
+                      )
+                    }
+
+                  })}
+                </SelectDropdown>
+
+                <SelectDropdown name="category" onChange={(event) => onChange(event)}>
+                <option disabled>
+                    Category
+                  </option>
+                  {categoryOptions.map((type) => {
+                    if (type.id === category) {
+                      return (
+                        <option selected key={type.id} value={type.id}>{type.categoryName}</option>
+                      )
+                    } else {
+                      return (
+                        <option key={type.id} value={type.id}>{type.categoryName}</option>
+                      )
+                    }
+                  })}
+                </SelectDropdown>
+              </div>
+
+              <div className="text-center lg:text-left">
+                <button
+                  type="submit"
+                  className="inline-block px-7 py-3 bg-amber-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-amber-700 hover:shadow-lg focus:bg-amber-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-amber-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                >
+                  Create Product
+                </button>
+              </div>
+
+            </div>
+
+            <div>
+              <p>Product Image</p>
+              <div className="flex justify-center items-center w-full">
+                <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-64 rounded-lg border-2 border-dashed cursor-pointer hover:border-amber-600">
+                  <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                    <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                  </div>
+                  <input id="dropzone-file" type="file" className="hidden" />
+                </label>
               </div>
             </div>
-          </form>
-        </section>
-      </PrivateLayout >
-    )
-  }
+          </div>
+        </form>
+      </section>
+    </PrivateLayout >
+  )
 }
