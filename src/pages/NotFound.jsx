eb5ec9901f20c5922f4483e58b8107f9282b2970
xShiro2 <daryllejumalon16@gmail.com>
2022-12-08 08:23:46 +0800

    feat: add not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function NotFound() {
     return (
-        <div>
-            <p>404 NOT FOUND</p>
+        <div className="max-w-screen text-center flex flex-col h-screen flex flex-col justify-center items-center">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-orange-600">404</h1>
+            <p className="mb-4 text-3xl tracking-tight font-bold text-gray-600 md:text-4xl">Something's missing.</p>
+            <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
         </div>
     )
 }
